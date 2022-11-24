9c4acaf08d55af3ef807b491097ef744ebc827da
Puzerevich <puzerevich@gmail.com>
2022-11-24 11:42:26 +0200

    Update GuessTheWord.java

--- src/Task_6/GuessTheWord.java
+++ src/Task_6/GuessTheWord.java
@@ -13,7 +13,7 @@ public class GuessTheWord {
     private String enterWord;
     private Scanner enter = new Scanner(System.in);
 
-    void startGm(){
+    public void startGm(){
         words = new String[]{"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
                 "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
                 "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
